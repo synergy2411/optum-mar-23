0ecbb7e47cc6dc9523ee3273f71c9a1dfd963a9f
Sumit Khandelwal <synergy2411@outlook.com>
2023-03-28 15:58:27 +0530

    hashed password

--- 02-graphql-yoga/src/graphql/resolvers/Mutation.js
+++ 02-graphql-yoga/src/graphql/resolvers/Mutation.js
@@ -1,4 +1,5 @@
 import { GraphQLError } from 'graphql';
+import { hash } from 'bcrypt';
 
 import UserModel from "../../model/user.model";
 
@@ -6,14 +7,17 @@ const Mutation = {
     registerUser: async (parent, args, context) => {
         try {
             const { email, password, age } = args.data;
-            const newUser = new UserModel({ email, password, age })
+            const hashPassword = await hash(password, 12)
+            const newUser = new UserModel({ email, password: hashPassword, age })
             const createdUser = await newUser.save()
             console.log("CREATED USER ", createdUser);
             return { message: "CREATED", email }
         } catch (err) {
+            console.log(err)
             throw new GraphQLError("Unable to created user " + err.message)
         }
     }
 }
 
 export { Mutation }
+
