84988d23e9a208bb7fe9a24b837a242543e90a03
Sumit Khandelwal <synergy2411@outlook.com>
2023-03-28 14:27:23 +0530

    yoga server created

--- 02-graphql-yoga/src/server.js
+++ 02-graphql-yoga/src/server.js
@@ -1,1 +1,27 @@
-console.log("Hello Yoga");
+import express from 'express';
+import { createYoga } from 'graphql-yoga';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+
+const app = express()
+
+const typeDefs = /* GraphQL */ `
+    type Query {
+        hello: String!
+    }
+`
+const resolvers = {
+    Query: {
+        hello: () => "World"
+    }
+}
+
+const schema = makeExecutableSchema({ typeDefs, resolvers })
+
+
+const yoga = createYoga({
+    schema
+})
+
+app.use("/graphql", yoga)
+
+app.listen(4001, () => console.log("Server running on PORT : 4001"))
