c83f1a7f3a5f2eb82fb5c45ec4a32b881b336899
Sumit Khandelwal <synergy2411@outlook.com>
2023-03-29 12:50:53 +0530

    token received

--- frontend/src/Components/Login/Login.jsx
+++ frontend/src/Components/Login/Login.jsx
@@ -1,4 +1,26 @@
+import { useMutation, gql } from "@apollo/client";
+
+const USER_LOGIN = gql`
+  mutation {
+    loginUser(data: { email: "foo@test", password: "foo123" }) {
+      token
+    }
+  }
+`;
+
 const Login = () => {
+  const [userLogin] = useMutation(USER_LOGIN);
+
+  const loginClickHandler = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await userLogin();
+      console.log("TOKEN : ", data.loginUser.token);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="row">
       <div className="col-6 offset-3">
@@ -23,7 +45,12 @@ const Login = () => {
               {/* buttons */}
               <div className="form-group">
                 <div className="d-grid">
-                  <button className="btn btn-primary">Login</button>
+                  <button
+                    className="btn btn-primary"
+                    onClick={loginClickHandler}
+                  >
+                    Login
+                  </button>
                 </div>
               </div>
             </form>
