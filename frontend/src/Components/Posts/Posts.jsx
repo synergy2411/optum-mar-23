5088a96d9a16536b2943b140e8d0536c0c13d29f
Sumit Khandelwal <synergy2411@outlook.com>
2023-03-29 12:32:51 +0530

    posts added

--- frontend/src/Components/Posts/Posts.jsx
+++ frontend/src/Components/Posts/Posts.jsx
@@ -1,7 +1,39 @@
+import { gql, useQuery } from "@apollo/client";
+
+const FETCH_POSTS = gql`
+  query {
+    posts {
+      id
+      title
+      body
+    }
+  }
+`;
+
 const Posts = () => {
+  const { data, loading, error } = useQuery(FETCH_POSTS);
+
+  if (loading) {
+    return <h3>Loading...</h3>;
+  }
+  if (error) {
+    return <h3>Something went wrong...</h3>;
+  }
+
   return (
     <div>
-      <h2>Posts coming soon...</h2>
+      <h2 className="text-center">All Posts</h2>
+      <div className="row">
+        {data.posts.map((post) => {
+          return (
+            <div className="col-3 m-2" key={post.id}>
+              <div className="card">
+                <div className="card-header">{post.title.toUpperCase()}</div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
